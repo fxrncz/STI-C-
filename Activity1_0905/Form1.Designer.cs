70248b0cfa0f71dc66f95d9c29669a199cb77c17
Francis Oliver <121164562+fxrncz@users.noreply.github.com>
2024-09-05 10:02:55 +0800

    Add files via upload

---
+++ Activity1_0905/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Activity1_0905
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.output = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Inter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Check Amount:";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Font = new System.Drawing.Font("Inter", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmount.Location = new System.Drawing.Point(33, 66);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(243, 26);
+            this.txtAmount.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Inter", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(308, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox
+            // 
+            this.imageBox.Image = global::Activity1_0905.Properties.Resources.blank_check;
+            this.imageBox.Location = new System.Drawing.Point(33, 144);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(339, 149);
+            this.imageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imageBox.TabIndex = 3;
+            this.imageBox.TabStop = false;
+            // 
+            // output
+            // 
+            this.output.AutoSize = true;
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(100, 204);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(13, 18);
+            this.output.TabIndex = 4;
+            this.output.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 361);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Label output;
+    }
+}
+
---
+++ Activity1_0905/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity1_0905
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int amount = Convert.ToInt32(txtAmount.Text);
+            string value1 = "";
+            string value2 = "";
+            //string value3 = "";
+
+            int tens = amount / 10;
+            int ones = amount - (tens * 10);
+
+            if(amount >= 11 || amount <= 19)
+            {
+                switch(amount)
+                {
+                    case 11:
+                        value1 = "Eleven";
+                        break;
+                    case 12:
+                        value1 = "Twelve";
+                        break;
+                    case 13:
+                        value1 = "Thirteen";
+                        break;
+                    case 14:
+                        value1 = "Fourteen";
+                        break;
+                    case 15:
+                        value1 = "Fifteen";
+                        break;
+                    case 16:
+                        value1 = "Sixteen";
+                        break;
+                    case 17:
+                        value1 = "Seventeen";
+                        break;
+                    case 18:
+                        value1 = "Eighteen";
+                        break;
+                    case 19:
+                        value1 = "Nineteen";
+                        break;
+                    
+                }
+            } else
+            {
+                switch(tens)
+                {
+                    // Tens
+                    case 1:
+                        value2 = "Ten";
+                        break;
+                    case 2:
+                        value2 = "Twenty";
+                        break;
+                    case 3:
+                        value2 = "Thirty";
+                        break;
+                    case 4:
+                        value2 = "Forty";
+                        break;
+                    case 5:
+                        value2 = "Fifthy";
+                        break;
+                    case 6:
+                        value2 = "Sixthy";
+                        break;
+                    case 7:
+                        value2 = "Seventhy";
+                        break;
+                    case 8:
+                        value2 = "Eighthy";
+                        break;
+                    case 9:
+                        value2 = "Ninety";
+                        break;
+                    
+                }
+
+                switch (ones)
+                {
+                    // Ones
+                    case 1:
+                        value1 = "Ten";
+                        break;
+                    case 2:
+                        value1 = "Twenty";
+                        break;
+                    case 3:
+                        value1 = "Thirty";
+                        break;
+                    case 4:
+                        value1 = "Forty";
+                        break;
+                    case 5:
+                        value1 = "Fifthy";
+                        break;
+                    case 6:
+                        value1 = "Sixthy";
+                        break;
+                    case 7:
+                        value1 = "Seventhy";
+                        break;
+                    case 8:
+                        value1 = "Eighthy";
+                        break;
+                    case 9:
+                        value1 = "Ninety";
+                        break;
+                    
+                }
+
+            }
+            
+
+            output.Text = value2 + " " + value1;
+        }
+    }
+}
